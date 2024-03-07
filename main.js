d52cdbc38baa761949cd017fb579fbe7a8a412f7
Bhatti149 <154119721+Bhatti149@users.noreply.github.com>
2024-03-08 00:35:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = 2;
+console.log(a + b);
+
+let c = 5;
+let d = 2;
+console.log(c - d);
+ let m1 = 5;
+ let m2 = 2;
+ console.log(m1 * m2);
+let d1 = 4;
+let d2 = 2;
+console.log(d1 / d2);
+let n2 = 10;
+let n3 = 3;
+console.log(n2 % n3);
